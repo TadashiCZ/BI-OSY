5ff5900db8b4eedf0eab29d0f8a5c6070ace109e
TadashiCZ <tadashicz@gmail.com>
2019-04-06 17:37:46 +0200

    rewritten, 1,1,P=k failing (not segfaulting)

--- progtest01/solution.cpp
+++ progtest01/solution.cpp
@@ -1,9 +1,3 @@
-#include <utility>
-
-#include <utility>
-
-#include <utility>
-
 #ifndef __PROGTEST__
 
 #include <cstdio>
@@ -37,82 +31,49 @@
 using namespace std;
 #endif /* __PROGTEST__ */
 
-class Problem {
+class Package {
 public:
 	ACustomer customer;
 	AOrderList orderList;
-
-	Problem() : customer( nullptr ), orderList( nullptr ) {}
-
-	Problem( ACustomer cust, AOrderList ordeL ) : customer( std::move( cust ) ), orderList( std::move( ordeL ) ) {}
-
-	Problem & operator=( const Problem & prob ) = default;
-
-	Problem( const Problem & prob ) {
-		this->customer = prob.customer;
-		this->orderList = prob.orderList;
-	}
 };
 
-class MaterialInfo {
+class Material {
 public:
-	MaterialInfo() {
-		counter = 99999999999;
-	}
-
-	MaterialInfo( const MaterialInfo & mater ) {
-		counter = mater.counter;
-		priceList = mater.priceList;
-	}
-
-	MaterialInfo( unsigned long cnt, APriceList pL ) : counter( cnt ), priceList( std::move( pL ) ) {}
-
-	unsigned long counter;
 	APriceList priceList;
-
-
+	set<AProducer> producers;
 };
 
 class CWeldingCompany {
 public:
 	static void SeqSolve( APriceList priceList, COrder & order );
-	void AddProducer( AProducer prod );
-	void AddCustomer( ACustomer cust );
+	void AddProducer( const AProducer & prod );
+	void AddCustomer( const ACustomer & cust );
 	void AddPriceList( AProducer prod, APriceList priceList );
 	void Start( unsigned thrCount );
-	void Stop();
-	void customerThreadFunction( const ACustomer & cust );
-	void workerThreadFunction();
-	APriceList checkForPriceList( unsigned materialID );
+	void Stop( void );
+	void customerThread( ACustomer cust );
+	void workerThread();
 
-	bool compareProducts( CProd & oldProd, CProd & newProd ) {
-		return ( ( oldProd.m_H == newProd.m_H && oldProd.m_W == newProd.m_W ) ||
-		         ( oldProd.m_W == newProd.m_H && oldProd.m_H == newProd.m_W ) );
-	}
-
-private:
-	map<unsigned, MaterialInfo> mPriceLists;
-	mutex mtx_priceList;
-	condition_variable cv_priceListExists;
-	condition_variable cv_priceListIsFull;
-
+	map<unsigned, Material> mPriceList;
+	queue<Package> mBuffer;
 	vector<AProducer> mProducers;
 	vector<ACustomer> mCustomers;
 
-	queue<Problem> mBuffer;
-	mutex mtx_buffer;
-	condition_variable cv_buffer;
-	condition_variable cv_bufferEmpty;
-
-	vector<thread> mWorkers;
+	vector<thread> mWorkerThreads;
 	vector<thread> mCustomerThreads;
 
-	long long int mActiveCustomers = -1;
-	mutex mtx_activeCustomers;
-	condition_variable cv_activeCustomers;
+	unsigned mThrCount;
+	unsigned long long int mDoneCustomers = 0;
+	unsigned bufferLimit = 10;
 
-	unsigned mThrCount = 0;
+	mutex mtx_Buffer;
+	mutex mtx_PriceList;
+	mutex mtx_Customers;
 
+	condition_variable cv_BufferEmpty;
+	condition_variable cv_BufferFull;
+	condition_variable cv_PriceListExists;
+	condition_variable cv_PriceListFull;
 
 };
 
@@ -122,272 +83,221 @@ private:
 	order = orderVector.front();
 }
 
-void CWeldingCompany::AddProducer( AProducer prod ) {
+void CWeldingCompany::AddProducer( const AProducer & prod ) {
 	mProducers.push_back( prod );
 }
 
-void CWeldingCompany::AddCustomer( ACustomer cust ) {
+void CWeldingCompany::AddCustomer( const ACustomer & cust ) {
 	mCustomers.push_back( cust );
 }
 
-void CWeldingCompany::AddPriceList( AProducer prod, APriceList priceList ) {
-	AProducer producer = prod;
-	{
-		unique_lock<mutex> lock( mtx_priceList );
-
-		auto it = mPriceLists.find( priceList->m_MaterialID );
-
-		if ( it == mPriceLists.end() ) {
-			mPriceLists.insert( pair<unsigned, MaterialInfo>(
-					priceList->m_MaterialID,
-					MaterialInfo( mCustomers.size(), make_shared<CPriceList>( priceList->m_MaterialID ) ) ) );
-		}
-
-		cv_priceListExists.notify_all();
-		MaterialInfo matInfo = mPriceLists.at( priceList->m_MaterialID );
-
-		for ( auto itNotYet = priceList->m_List.begin() ; itNotYet != priceList->m_List.end() ; ++itNotYet ) {
-			for ( auto & itAlready : matInfo.priceList->m_List ) {
-				if ( compareProducts( itAlready, *itNotYet ) ) {
-					if ( itAlready.m_Cost <= itNotYet->m_Cost ) {
-						continue;
-					} else {
-						itAlready.m_Cost = itNotYet->m_Cost;
-						continue;
-					}
-				}
-			}
-			mPriceLists[priceList->m_MaterialID].priceList->m_List.push_back( *itNotYet );
-		}
-
-		mPriceLists.at( priceList->m_MaterialID ).counter--;
-		cv_priceListIsFull.notify_all();
-	}
-}
-
 void CWeldingCompany::Start( unsigned thrCount ) {
-	// init part
-	mActiveCustomers = mCustomers.size();
-
-	this->mThrCount = thrCount;
-
-	mWorkers.reserve( this->mThrCount );
-	mCustomerThreads.reserve( mCustomers.size() );
+	mThrCount = thrCount;
 
 	for ( auto & mCustomer : mCustomers ) {
-		mCustomerThreads.emplace_back( &CWeldingCompany::customerThreadFunction, this, ref( mCustomer ) );
-	}
-
-	for ( unsigned i = 0 ; i < thrCount ; ++i ) {
-		mWorkers.emplace_back( &CWeldingCompany::workerThreadFunction, this );
-	}
-
-}
-
-APriceList CWeldingCompany::checkForPriceList( unsigned materialID ) {
-	unique_lock<mutex> lock( mtx_priceList );
-	if ( mPriceLists.find( materialID ) != mPriceLists.end() ) {
-		if ( mPriceLists.at( materialID ).counter == 0 ) {
-			return mPriceLists.at( materialID ).priceList;
-		}
-	} else {
-		cv_priceListExists.wait( lock, [&] { return mPriceLists.find( materialID ) != mPriceLists.end(); } );
-		cv_priceListIsFull.wait( lock, [&] { return mPriceLists[materialID].counter == 0; } );
-	}
-
-	return mPriceLists.at( materialID ).priceList;
-
-}
-
-void CWeldingCompany::Stop() {
-	{
-		unique_lock<mutex> lock( mtx_activeCustomers );
-		cv_activeCustomers.wait( lock, [&] { return mActiveCustomers == 0; } );
-	}
-
-	for ( auto & customer : mCustomerThreads ) {
-		customer.join();
+		mCustomerThreads.emplace_back( &CWeldingCompany::customerThread, this, ref( mCustomer ) );
 	}
 
 	for ( unsigned i = 0 ; i < mThrCount ; ++i ) {
-		cv_bufferEmpty.notify_all();
-		cv_buffer.notify_all();
-
+		mWorkerThreads.emplace_back( &CWeldingCompany::workerThread, this );
 	}
 
-	for ( auto & worker : mWorkers ) {
-		worker.join();
-	}
+	//cout << "START: WORKERS: " << thrCount << ", CUSTOMERS: " << mCustomers.size() << endl;
+}
 
+void CWeldingCompany::AddPriceList( AProducer prod, APriceList priceList ) {
+	unsigned matID = priceList->m_MaterialID;
+	{
+		unique_lock<mutex> lockExists( mtx_PriceList );
+		auto it = mPriceList.find( matID );
+
+		if ( it == mPriceList.end() ) {
+			//	cout << this_thread::get_id() << ":ADD_PRICE_LIST: priceList for ID: " << matID << " not found\n";
+			make_shared<CPriceList>( matID );
+			Material newMat;
+			newMat.priceList = priceList;
+			newMat.producers.insert( prod );
+			//cout << this_thread::get_id() << ":ADD_PRICE_LIST: new producer in priceList (now " << newMat.producers.size() << ")\n";
+			mPriceList.insert( pair<unsigned, Material>( matID, newMat ) );
+			cv_PriceListExists.notify_all();
+			cv_PriceListFull.notify_all();
+			return;
+		}
+	}
+	//cout << this_thread::get_id() << ":ADD_PRICE_LIST: material with ID: " << priceList->m_MaterialID	     << " already in priceList\n";
+	cv_PriceListExists.notify_all();
+	{
+		unique_lock<mutex> lockExists( mtx_PriceList );
+
+		if ( mPriceList.at( matID ).producers.count( prod ) != 0 ) {
+			//cout << this_thread::get_id() << ":ADD_PRICE_LIST: priceList for this producer already exists\n";
+			return;
+		}
+		mPriceList.at( matID ).producers.insert( prod );
+		//cout << this_thread::get_id() << ":ADD_PRICE_LIST: new producer in priceList (now " << mPriceList.at( matID ).producers.size() << ")\n";
+		for ( auto newProd : priceList->m_List ) {
+			bool isNew = true;
+			for ( auto oldProd : mPriceList.at( matID ).priceList->m_List ) {
+				if ( ( newProd.m_W == oldProd.m_W && newProd.m_H == oldProd.m_H ) ||
+				     ( newProd.m_W == oldProd.m_H && newProd.m_H == oldProd.m_W ) ) {
+					isNew = false;
+					//cout << this_thread::get_id() << ":ADD_PRICE_LIST: found same size product\n";
+					if ( newProd.m_Cost < oldProd.m_Cost ) {
+						//	cout << this_thread::get_id() << ":ADD_PRICE_LIST: new product is cheaper\n";
+						oldProd = newProd;
+					}
+				}
+			}
+			if ( isNew ) {
+				mPriceList.at( matID ).priceList->Add( newProd );
+			}
+		}
+
+	}
+//	cout << this_thread::get_id() << ":ADD_PRICE_LIST: notifying about full priceLists\n";
+	cv_PriceListFull.notify_all();
+//	cout << this_thread::get_id() << ":ADD_PRICE_LIST: END OF FUNCTION\n";
 
 }
 
-void CWeldingCompany::customerThreadFunction( const ACustomer & cust ) {
-	cout << "Start customerThreadFunction\n";
+void CWeldingCompany::customerThread( ACustomer cust ) {
+	//cout << this_thread::get_id() << ":CUST: START\n";
 	while ( true ) {
-		// get orderList
 		AOrderList orderList = cust->WaitForDemand();
 		if ( orderList.get() == nullptr ) {
+			//	cout << this_thread::get_id() << ":CUST: stop customer thread, customer finished\n";
 			{
-				unique_lock<mutex> lock( mtx_activeCustomers );
-				cv_activeCustomers.notify_all();
-				mActiveCustomers--;
+				unique_lock<mutex> lockCust( mtx_Customers );
+				++mDoneCustomers;
 			}
 			break;
 		}
 
-		for ( auto & producer :  mProducers ) {
-			producer->SendPriceList( orderList->m_MaterialID );
-		}
+		Package pack = Package();
+		pack.orderList = orderList;
+		pack.customer = cust;
 
-		//create Problem and put it into Buffer
-		Problem prob = Problem( cust, orderList );
-/*
-		cout << "Put problem into buffer:\nOrders:\n";
-		for ( unsigned i = 0 ; i < orderList->m_List.size() ; ++i ) {
-			cout << "m_W: " << orderList->m_List[i].m_W << ", m_H: " << orderList->m_List[i].m_H << ", m_S: "
-			     << orderList->m_List[i].m_WeldingStrength << "\n";
-		}*/
+		for ( auto & mProducer : mProducers ) {
+			mProducer->SendPriceList( pack.orderList->m_MaterialID );
+		}
 
 		{
-			unique_lock<mutex> lock( mtx_buffer );
-			mBuffer.push( prob );
+			unique_lock<mutex> lockFull( mtx_Buffer );
+			cv_BufferFull.wait( lockFull, [&] { return mBuffer.size() < bufferLimit; } );
+			//cout << this_thread::get_id() << ":CUST: push package into buffer\n";
+			mBuffer.push( pack );
 		}
 
-		cv_buffer.notify_all();
+		//cout << this_thread::get_id() << ":CUST: notyfing about added package\n";
+		cv_BufferEmpty.notify_all();
 	}
-
+//	cout << this_thread::get_id() << ":CUST: END OF FUNCTION\n";
 }
 
-void CWeldingCompany::workerThreadFunction() {
-	cout << "Start workerThreadFunction\n";
+void CWeldingCompany::workerThread() {
+//	cout << this_thread::get_id() << ":WORK: START\n";
 	while ( true ) {
-
 		{
-			unique_lock<mutex> lock1( mtx_activeCustomers );
-			unique_lock<mutex> lock2( mtx_buffer );
-			if ( mActiveCustomers == 0 && mBuffer.empty() ) {
+			unique_lock<mutex> lock( mtx_Buffer );
+			if ( mBuffer.empty() && mDoneCustomers == mCustomers.size() ) {
+				//	cout << this_thread::get_id() << ":WORK: stop worker thread, all customers finished, empty buffer\n";
+				cv_BufferEmpty.notify_all();
 				break;
 			}
 		}
 
-		Problem prob;
 		{
-			unique_lock<mutex> lock( mtx_buffer );
-			cv_bufferEmpty.wait( lock, [&] { return !mBuffer.empty(); } );
+			unique_lock<mutex> lockEmpty( mtx_Buffer );
+			cv_BufferEmpty.wait( lockEmpty, [&] {
+				//		cout << this_thread::get_id() << ":WORK: waiting with empty buffer\n";
+				return !( mBuffer.empty() && mDoneCustomers != mCustomers.size() );
+			} );
 		}
 
+		Package pack;
+		{
+			unique_lock<mutex> lockEmpty( mtx_Buffer );
+			if ( mBuffer.empty() ) {
+				continue;
+			}
+			pack = mBuffer.front();
+			mBuffer.pop();
+		}
+
+		cv_BufferFull.notify_one();
+
+		for ( auto & producer : mProducers ) {
+			producer->SendPriceList( pack.orderList->m_MaterialID );
+		}
 
 		{
-			unique_lock<mutex> lock( mtx_buffer );
-			if ( !mBuffer.empty() ) {
-				prob = mBuffer.front();
-				mBuffer.pop();
-			}
+			unique_lock<mutex> lockExist( mtx_PriceList );
+			cv_PriceListExists.wait( lockExist, [&] {
+				//		cout << this_thread::get_id() << ":WORK: waiting for priceList existence\n";
+				return mPriceList.find( pack.orderList->m_MaterialID ) != mPriceList.end();
+			} );
 		}
 
-		APriceList priceList = checkForPriceList( prob.orderList->m_MaterialID );
+		{
+			unique_lock<mutex> lockFull( mtx_PriceList );
+			cv_PriceListFull.wait( lockFull, [&] {
+				//	cout << this_thread::get_id() << ":WORK: waiting for priceList filling: producers: " << mProducers.size()				     << ", prodSize: " << mPriceList.at( pack.orderList->m_MaterialID ).producers.size() << "\n";
+				return mPriceList.at( pack.orderList->m_MaterialID ).producers.size() >= mProducers.size();
+			} );
+		}
 
-		/*cout << "PriceList " << priceList->m_MaterialID << ":\n";
-		if ( priceList->m_List.empty() ) {
-			cout << "empty price list\n";
-		} else {
-			for ( unsigned j = 0 ; j < priceList->m_List.size() ; ++j ) {
-				cout << "m_W: " << priceList->m_List[j].m_W << ", m_H: " << priceList->m_List[j].m_H << ", m_Cost: "
-				     << priceList->m_List[j].m_Cost << "\n";
-			}
-			cout << "\n\n";
+		Material material;
+		{
+			unique_lock<mutex> lockPrice( mtx_PriceList );
+			material = mPriceList.at( pack.orderList->m_MaterialID );
 		}
-		cout << "Call solver\n";*/
-		ProgtestSolver( prob.orderList->m_List, priceList );
-		/*cout << "Solver result: MaterialID: " << prob.orderList->m_MaterialID << ", prices:" << endl;
-		for ( unsigned i = 0 ; i < prob.orderList->m_List.size() ; ++i ) {
-			cout << "Order " << i << ": " << prob.orderList->m_List[i].m_Cost << endl;
+
+		//	cout << this_thread::get_id() << ":WORK: solver called\n";
+		ProgtestSolver( pack.orderList->m_List, material.priceList );
+		//	cout << this_thread::get_id() << ":WORK: solver finished\n";
+
+		{
+			unique_lock<mutex> lockCust( mtx_Customers );
+			pack.customer->Completed( pack.orderList );
 		}
-		cout << endl;*/
-		prob.customer->Completed( prob.orderList );
+		//	cout << this_thread::get_id() << ":WORK: order with ID " << pack.orderList->m_MaterialID << " completed\n";
 	}
+
+//	cout << this_thread::get_id() << ":WORK: END OF FUNCTION\n";
+}
+
+void CWeldingCompany::Stop() {
+	for ( unsigned i = 0 ; i < mCustomers.size() ; ++i ) {
+		mCustomerThreads[i].join();
+	}
+
+	cv_BufferEmpty.notify_all();
+	cv_PriceListFull.notify_all();
+	cv_BufferFull.notify_all();
+	cv_PriceListExists.notify_all();
+
+	for ( unsigned j = 0 ; j < mThrCount ; ++j ) {
+		mWorkerThreads[j].join();
+	}
+
 }
 
 //-------------------------------------------------------------------------------------------------
 #ifndef __PROGTEST__
 
-int main() {
+int main( void ) {
 	using namespace std::placeholders;
-	CWeldingCompany test, test2;
-/*
+	CWeldingCompany test;
 
 	AProducer p1 = make_shared<CProducerSync>( bind( &CWeldingCompany::AddPriceList, &test, _1, _2 ) );
 	AProducerAsync p2 = make_shared<CProducerAsync>( bind( &CWeldingCompany::AddPriceList, &test, _1, _2 ) );
 	test.AddProducer( p1 );
 	test.AddProducer( p2 );
-	test.AddCustomer( make_shared<CCustomerTest>( 1 ) );
+	test.AddCustomer( make_shared<CCustomerTest>( 2 ) );
 	p2->Start();
 	test.Start( 3 );
 	test.Stop();
 	p2->Stop();
-*/
-
-
-	AProducer p3 = make_shared<CProducerSync>( bind( &CWeldingCompany::AddPriceList, &test2, _1, _2 ) );
-	AProducer p6 = make_shared<CProducerSync>( bind( &CWeldingCompany::AddPriceList, &test2, _1, _2 ) );
-	AProducer p4 = make_shared<CProducerSync>( bind( &CWeldingCompany::AddPriceList, &test2, _1, _2 ) );
-	AProducer p5 = make_shared<CProducerSync>( bind( &CWeldingCompany::AddPriceList, &test2, _1, _2 ) );
-	test2.AddProducer( p3 );
-	test2.AddProducer( p4 );
-	test2.AddProducer( p5 );
-	test2.AddProducer( p6 );
-	AProducer p7 = make_shared<CProducerSync>( bind( &CWeldingCompany::AddPriceList, &test2, _1, _2 ) );
-	AProducerAsync p8 = make_shared<CProducerAsync>( bind( &CWeldingCompany::AddPriceList, &test2, _1, _2 ) );
-	AProducerAsync p9 = make_shared<CProducerAsync>( bind( &CWeldingCompany::AddPriceList, &test2, _1, _2 ) );
-	AProducerAsync p10 = make_shared<CProducerAsync>( bind( &CWeldingCompany::AddPriceList, &test2, _1, _2 ) );
-	AProducerAsync p11 = make_shared<CProducerAsync>( bind( &CWeldingCompany::AddPriceList, &test2, _1, _2 ) );
-
-	AProducerAsync p12 = make_shared<CProducerAsync>( bind( &CWeldingCompany::AddPriceList, &test2, _1, _2 ) );
-	test2.AddProducer( p9 );
-	test2.AddProducer( p10 );
-	test2.AddProducer( p7 );
-	test2.AddProducer( p11 );
-	test2.AddProducer( p12 );
-	test2.AddProducer( p8 );
-	test2.AddCustomer( make_shared<CCustomerTest>( 20 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 4 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 24 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 9 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 20 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 8 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 24 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 9 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 20 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 4 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 24 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 9 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 4 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 24 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 9 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 20 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 8 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 24 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 9 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 20 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 4 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 24 ) );
-	test2.AddCustomer( make_shared<CCustomerTest>( 9 ) );
-	p8->Start();
-	p9->Start();
-	p10->Start();
-	p11->Start();
-	p12->Start();
-	test2.Start( 4 );
-	test.Stop();
-	p12->Stop();
-	p11->Stop();
-	p10->Stop();
-	p9->Stop();
-	p8->Stop();
-	cout << "FINISHED" << endl;
-
-
 	return 0;
 }
 
